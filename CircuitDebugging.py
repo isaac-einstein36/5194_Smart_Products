fe6839a45b9c52cbe3f6997f85a9c8025d78a6a8
Isaac Einstein <einstein.15@osu.edu>
2025-02-06 15:03:50 -0500

    debugging code works for buttons, LED, and buzzer

--- CircuitDebugging.py
+++ CircuitDebugging.py
@@ -1,9 +1,37 @@
 from gpiozero import Buzzer, Button, LED
+from gpiozero import PWMOutputDevice
 from time import sleep
 
+def playTone(frequency, duration):
+    buzzer.frequency = frequency
+    buzzer.value = 0.5
+
+    sleep(duration)
+
+    buzzer.value = 0
+
 def testFunction():
-    print("Hello World")
+    global ledOn
+    ledOn = not ledOn
+
+    if ledOn:
+        testLED.on()
+    else:
+        testLED.off()
+
+    playTone(1000, 1)
+
 
 # Declare button
 greenButton = Button(17)
 greenButton.when_pressed = testFunction
+
+# Declare led
+testLED = LED(22)
+ledOn = True
+
+# Buzzer Stuff
+buzzer = PWMOutputDevice(19)
+
+while True:
+    x = 5
